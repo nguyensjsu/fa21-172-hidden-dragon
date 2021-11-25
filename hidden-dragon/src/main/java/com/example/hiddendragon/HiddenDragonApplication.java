4b03738ef855526311cdcaafe2ae9e977199afb7
Alan Le <underrated@gmail.com>
2021-11-25 15:35:47 -0800

    [1.0.0] Spring Boot setup

---
+++ hidden-dragon/src/main/java/com/example/hiddendragon/HiddenDragonApplication.java
@@ -0,0 +1,13 @@
+package com.example.hiddendragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiddenDragonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiddenDragonApplication.class, args);
+	}
+
+}
---
+++ hidden-dragon/src/test/java/com/example/hiddendragon/HiddenDragonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hiddendragon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiddenDragonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
