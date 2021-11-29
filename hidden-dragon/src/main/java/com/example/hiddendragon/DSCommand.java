8b6c3a0c1041a9405e20af361bf28695d83c10b8
Brandon Cheu <brandon.cheu@sjsu.edu>
2021-11-28 21:35:34 -0800

    Added payment info to command class

--- hidden-dragon/src/main/java/com/example/hiddendragon/DSCommand.java
+++ hidden-dragon/src/main/java/com/example/hiddendragon/DSCommand.java
@@ -1,5 +1,29 @@
 package com.example.hiddendragon;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
-public class DSCommand {
-    
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "drugStore_command")
+@Data
+@RequiredArgsConstructor
+class DSCommand {
+    private @Id @GeneratedValue Long id; 
+    private String action ;
+    private String firstname ;
+    private String lastname ;
+    private String notes;
+    private String phone;
+    private String state;
+    private String zip;
+    private String email;
+    private String city;
+    private String cardnum;
+    private String cardexpyear;
+    private String cardcvv;
+    private String address;
 }
