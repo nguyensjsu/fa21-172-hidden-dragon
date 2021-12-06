e2e8d0fb02aea7f14b45a470415df4f400018724
Alan Le <underrated@gmail.com>
2021-12-06 15:51:14 -0800

    [1.0.8] K8 deployment files and data seeding

---
+++ hidden-dragon/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+
+-- MySQL Sample Data / BIOS SCHEMA
+
+-- Item
+
+INSERT INTO item ( id, name,  price, stock, image ) VALUES	
+(1,'Mask', 1.00, 10, "mask.jpg"),
+(2,'Bandaid', 2.00, 50, "Bandaid.jpg"),
+(3,'Advil', 3.50, 150, "advil.png"),
+(4,'Neosporin', 4.00, 75, "neo.jpg") ;
